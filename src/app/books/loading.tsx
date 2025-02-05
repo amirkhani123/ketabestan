5d543d0c9538d2ba9580f153357a2319d3adc4ac
amirKhani <ww.amirkhani.i@gmail.com>
2025-02-05 13:09:00 +0330

    add new loading for booksPage and DeatilsBookPage

---
+++ src/app/books/loading.tsx
@@ -0,0 +1,10 @@
+export default function loadingBookDetails() {
+    return (
+      <div className="w-full h-[55vh] flex items-center justify-center">
+          <div className="size-28 rounded-[50%] border-[6px] border-r-transparent border-l-transparent border-dotted border-gray-600 my-hover animate-spin ">
+          </div>
+      </div>
+    )
+  }
+  
+  

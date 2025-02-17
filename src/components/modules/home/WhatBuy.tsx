baa56361c3cfcd7bbc0dc0aa1ac6403c11d4b103
amirKhani <ww.amirkhani.i@gmail.com>
2025-02-17 17:55:38 +0330

    update responsive HomaPage

--- src/components/modules/home/WhatBuy.tsx
+++ src/components/modules/home/WhatBuy.tsx
@@ -3,9 +3,9 @@ import { Fade } from "react-awesome-reveal"
 function WhatBuy() {
   return (
     <div className="w-full mt-8">
-        <h4 className="font-normal text-2xl my-3">چگونه کتاب مورد علاقمو بخرم ؟ 🤔</h4>
+        <h4 className="font-normal text-2xl my-3 max-md:text-lg">چگونه کتاب مورد علاقمو بخرم ؟ 🤔</h4>
         <Fade cascade direction="up" triggerOnce>
-        <ul className="list-decimal mr-5">
+        <ul className="list-decimal pr-5 max-sm:pr-7 w-full">
             <li className="font-light text-xl">ثبت نام در سایت </li>
             <li className="font-light text-xl">پیدا کردن کتاب مورد علاقه</li>
             <li className="font-light text-xl">اضافه در سبد خرید</li>
--- src/components/modules/home/Why.tsx
+++ src/components/modules/home/Why.tsx
@@ -6,23 +6,23 @@ function Why() {
   return (
     <div className="w-full my-2 ">
         <h3 className="font-normal text-2xl ">چرا ما ؟ </h3>
-        <div className="flex items-center justify-between gap-2 py-3 max-sm:flex-wrap">
+        <div className="flex items-center justify-between gap-2 py-3 max-sm:flex-wrap max-sm:gap-1 max-md:mx-1 ">
         <Fade direction="up" cascade triggerOnce>
-            <div className="flex flex-col items-center p-3 rounded-2xl bg-white justify-between   size-44 ">
+            <div className="flex flex-col items-center p-3 rounded-2xl bg-white justify-between size-44  max-md:size-36 ">
             <IoPricetagOutline size={65}  />
-                <p className="font-light text-xl ">قیمت مناسب </p>
+                <p className="font-light text-xl max-md:text-lg ">قیمت مناسب </p>
             </div>
-            <div className="flex flex-col items-center p-3 rounded-2xl bg-white justify-between size-44 ">
+            <div className="flex flex-col items-center p-3 rounded-2xl bg-white justify-between size-44 max-md:size-36">
             <MdOutlineSupportAgent size={65}/>
-              <p className="font-light text-lg ">پشتیبانی 24 ساعته </p>
+              <p className="font-light text-lg   max-md:text-[19px]">پشتیبانی 24 ساعته </p>
             </div>
-            <div className="flex flex-col items-center p-3 rounded-2xl bg-white justify-between size-44 ">
+            <div className="flex flex-col items-center p-3 rounded-2xl bg-white justify-between size-44 max-md:size-36 ">
             <CiDeliveryTruck size={65} />
-                <p className="font-light text-xl "> ارسال رایگان</p>
+                <p className="font-light text-xl  max-md:text-lg "> ارسال رایگان</p>
             </div>
-            <div className="flex flex-col items-center p-3 rounded-2xl bg-white justify-between size-44 ">
+            <div className="flex flex-col items-center p-3 rounded-2xl bg-white justify-between size-44 max-md:size-36">
             <CiDiscount1 size={65} />
-               <p className="font-light text-xl ">تخفیفات متنوع</p>
+               <p className="font-light text-xl  max-md:text-lg ">تخفیفات متنوع</p>
             </div>
             </Fade>
         </div>
